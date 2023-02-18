f19cc01ae4013e8233cb8f9effc5b84624dfa41c
Igor Kuzmenkov <me@igorduino.ru>
2023-02-18 10:40:01 +0300

    logic: allow trial users to add new profile if old was removed

--- tgbot/handlers/onboarding/handlers.py
+++ tgbot/handlers/onboarding/handlers.py
@@ -224,9 +224,10 @@ def pay_handler(update: Update, context: CallbackContext) -> None:
 def new_profile_handler(update: Update, context: CallbackContext) -> None:
     user = User.get_user(update, context)
     if user.is_trial:
-        update.callback_query.edit_message_text(
-            shop_text.new_profile_trial_text, reply_markup=keyboards.main_menu(user))
-        return
+        if VPNProfile.objects.filter(user=user).count() > 0:
+            update.callback_query.edit_message_text(
+                shop_text.new_profile_trial_text, reply_markup=keyboards.main_menu(user))
+            return
     if VPNProfile.objects.filter(user=user).count() >= MAXIMUM_PROFILES:
         update.callback_query.edit_message_text(
             shop_text.maximum_profiles_text, reply_markup=keyboards.main_menu(user))
